8991406e906a7ed8e9090fe78916cc61231004f2
Talha <pateltalha934@gmail.com>
2026-03-25 11:28:10 +0530

    get-all using pagination

--- Spring_Boot/MyApp/src/main/java/com/springboot/myapp/controller/TicketController.java
+++ Spring_Boot/MyApp/src/main/java/com/springboot/myapp/controller/TicketController.java
@@ -1,5 +1,6 @@
 package com.springboot.myapp.controller;
 
+import com.springboot.myapp.dto.TicketPageResDto;
 import com.springboot.myapp.dto.TicketReqDto;
 import com.springboot.myapp.model.Ticket;
 import com.springboot.myapp.service.TicketService;
@@ -7,18 +8,25 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/ticket")
 public class TicketController {
     private final TicketService ticketService;
 
-    @PostMapping("/api/ticket/add")
+    @PostMapping("/add")
     public ResponseEntity<?> addTicket(@Valid @RequestBody TicketReqDto ticketReqDto){
         ticketService.addTicket(ticketReqDto); //this will give json body
         return ResponseEntity.status(201).build(); //this will hide and will not give anything
+    }
+
+    @GetMapping("/get-all")
+    public TicketPageResDto getAllTickets(@RequestParam (value = "page", required = false, defaultValue = "0") int page,
+                                          @RequestParam (value = "size", required = false, defaultValue = "0") int size){
+        return ticketService.getAllTickets(page,size);
     }
 }
--- Spring_Boot/MyApp/src/main/java/com/springboot/myapp/service/TicketService.java
+++ Spring_Boot/MyApp/src/main/java/com/springboot/myapp/service/TicketService.java
@@ -1,5 +1,6 @@
 package com.springboot.myapp.service;
 
+import com.springboot.myapp.dto.TicketPageResDto;
 import com.springboot.myapp.dto.TicketReqDto;
 import com.springboot.myapp.enums.TicketStatus;
 import com.springboot.myapp.mapper.TicketMapper;
@@ -7,14 +8,18 @@ import com.springboot.myapp.model.Ticket;
 import com.springboot.myapp.repository.TicketRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class TicketService {
     private final TicketRepository ticketRepository;
 
-    public Ticket addTicket(@Valid TicketReqDto ticketReqDto) {
+    public void addTicket(@Valid TicketReqDto ticketReqDto) {
         //1. map dto to entity using mapper
         Ticket ticket = TicketMapper.mapToEntity(ticketReqDto);
 
@@ -22,6 +27,22 @@ public class TicketService {
         ticket.setTicketStatus(TicketStatus.OPEN);
 
         //3. add to db
-        return ticketRepository.save(ticket);
+        ticketRepository.save(ticket);
+    }
+
+    public TicketPageResDto getAllTickets(int page, int size) {
+        // object of pageable and pass page and size
+        Pageable pageable = PageRequest.of(page,size);
+        //starting id of page = page*size+1
+
+        Page<Ticket> pageTicket = ticketRepository.findAll(pageable);
+        long totalRecords = pageTicket.getTotalElements();
+        int totalPages = pageTicket.getTotalPages();
+
+        return new TicketPageResDto(
+                pageTicket.toList(),
+                totalRecords,
+                totalPages
+        );
     }
 }
