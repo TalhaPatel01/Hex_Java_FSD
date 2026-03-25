8addfe47240a67ed54f331362af08cd88d522140
Talha <pateltalha934@gmail.com>
2026-03-25 17:47:14 +0530

    done all 3 GET APIs for executive

--- Spring_Boot/MyApp/src/main/java/com/springboot/myapp/controller/ExecutiveController.java
+++ Spring_Boot/MyApp/src/main/java/com/springboot/myapp/controller/ExecutiveController.java
@@ -1,23 +1,43 @@
 package com.springboot.myapp.controller;
 
+import com.springboot.myapp.dto.ExecutivePageResDto;
 import com.springboot.myapp.dto.ExecutiveReqDto;
+import com.springboot.myapp.enums.JobTitle;
+import com.springboot.myapp.model.Executive;
 import com.springboot.myapp.service.ExecutiveService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/executive")
 public class ExecutiveController {
     private final ExecutiveService executiveService;
 
-    @PostMapping("/api/executive/add")
+    @PostMapping("/add")
     public ResponseEntity<?> addExecutive(@Valid @RequestBody ExecutiveReqDto executiveReqDto){
         executiveService.addExecutive(executiveReqDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/get-all")
+    public ExecutivePageResDto getAllExecutives(@RequestParam (value="page", required = false, defaultValue = "0") int page,
+                                                @RequestParam (value = "size", required = false, defaultValue = "0") int size){
+        return executiveService.getAllExecutives(page,size);
+    }
+
+    @GetMapping("/get/{id}")
+    public Executive getExecutiveById(@PathVariable long id){
+        return executiveService.getExecutiveById(id);
+    }
+
+    @GetMapping("/get/filter/{jobTitle}")
+    public List<Executive> getExecutiveByJobTitle(@PathVariable JobTitle jobTitle){
+        return executiveService.getExecutiveByJobTitle(jobTitle);
     }
 }
--- Spring_Boot/MyApp/src/main/java/com/springboot/myapp/service/ExecutiveService.java
+++ Spring_Boot/MyApp/src/main/java/com/springboot/myapp/service/ExecutiveService.java
@@ -1,17 +1,27 @@
 package com.springboot.myapp.service;
 
+import com.springboot.myapp.dto.ExecutivePageResDto;
 import com.springboot.myapp.dto.ExecutiveReqDto;
+import com.springboot.myapp.enums.JobTitle;
+import com.springboot.myapp.exception.ResourceNotFoundException;
 import com.springboot.myapp.mapper.ExecutiveMapper;
 import com.springboot.myapp.model.Executive;
 import com.springboot.myapp.repository.ExecutiveRepository;
+import com.springboot.myapp.repository.TicketRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class ExecutiveService {
     private final ExecutiveRepository executiveRepository;
+    private final TicketRepository ticketRepository;
 
     public void addExecutive(@Valid ExecutiveReqDto executiveReqDto) {
         //1. map dto to entity
@@ -19,5 +29,32 @@ public class ExecutiveService {
 
         //2. add to db
         executiveRepository.save(executive);
+    }
+
+    public ExecutivePageResDto getAllExecutives(int page, int size) {
+        Pageable pageable = PageRequest.of(page,size);
+        Page<Executive> pageTicket = executiveRepository.findAll(pageable);
+        long totalRecords = pageTicket.getTotalElements();
+        int totalPages = pageTicket.getTotalPages();
+
+        List<Executive> list = pageTicket.stream().toList();
+
+        return new ExecutivePageResDto(
+               list,
+               totalRecords,
+               totalPages
+        );
+    }
+
+    public Executive getExecutiveById(long id) {
+        return executiveRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Invalid id"));
+    }
+
+    public List<Executive> getExecutiveByJobTitle(JobTitle jobTitle) {
+        if(jobTitle==null){
+            return List.of();
+        }
+        return executiveRepository.getExecutiveByJobTitle(jobTitle);
     }
 }
