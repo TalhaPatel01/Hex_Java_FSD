468579e9391606fcae27853ab70155529f941d9b
Talha <pateltalha934@gmail.com>
2026-03-31 13:30:14 +0530

    Testing+mocking - get all with pagination

--- Spring_Boot/MyApp/src/test/java/com/springboot/myapp/service/TicketServiceTest.java
+++ Spring_Boot/MyApp/src/test/java/com/springboot/myapp/service/TicketServiceTest.java
@@ -13,8 +13,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
@@ -80,5 +85,43 @@ public class TicketServiceTest {
 
         //check for exception message
         Assertions.assertEquals("Invalid id given",e.getMessage());
+    }
+
+    @Test
+    public void getAllTicketsTest(){
+        /* Prepare the List. */
+        Ticket ticket1 = new Ticket();
+        ticket1.setId(12L);
+        ticket1.setSubject("test subject");
+        ticket1.setTicketPriority(TicketPriority.LOW);
+        ticket1.setTicketStatus(TicketStatus.OPEN);
+        ticket1.setCreatedAt(Instant.now());
+        Ticket ticket2 = new Ticket();
+        ticket2.setId(14L);
+        ticket2.setSubject("test subject");
+        ticket2.setTicketPriority(TicketPriority.HIGH);
+        ticket2.setTicketStatus(TicketStatus.CLOSED);
+        ticket2.setCreatedAt(Instant.now());
+        List<Ticket> list = List.of(ticket1,ticket2);
+
+        //create page obj
+        Page<Ticket> pageTicket = new PageImpl<>(list);
+        int page=0;
+        int size=2;
+
+        Pageable pageable = PageRequest.of(page,size);
+
+        //mocking repository call
+        when(ticketRepository.findAll(pageable)).thenReturn(pageTicket);
+        Assertions.assertEquals(2,ticketService.getAllTickets(0,2).list().size());
+
+        Page<Ticket> pageTicket1 = new PageImpl<>(list.subList(0,1));
+        page = 0;
+        size = 1;
+
+        Pageable pageable1 = PageRequest.of(page,size);
+        // Mock the repository call for findALL()
+        when(ticketRepository.findAll(pageable1)).thenReturn(pageTicket1);
+        Assertions.assertEquals(1,ticketService.getAllTickets(0,1).list().size());
     }
 }
