5974a474c19fb17acfe31eb12fefd5708613bbc4
Talha <pateltalha934@gmail.com>
2026-03-31 12:16:14 +0530

    Mocking + testing - when by id not found

--- Spring_Boot/MyApp/src/test/java/com/springboot/myapp/service/TicketServiceTest.java
+++ Spring_Boot/MyApp/src/test/java/com/springboot/myapp/service/TicketServiceTest.java
@@ -3,6 +3,7 @@ package com.springboot.myapp.service;
 import com.springboot.myapp.dto.TicketResDto;
 import com.springboot.myapp.enums.TicketPriority;
 import com.springboot.myapp.enums.TicketStatus;
+import com.springboot.myapp.exception.ResourceNotFoundException;
 import com.springboot.myapp.model.Ticket;
 import com.springboot.myapp.repository.TicketRepository;
 import org.junit.jupiter.api.Assertions;
@@ -15,6 +16,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
 import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TicketServiceTest {
@@ -39,7 +42,7 @@ public class TicketServiceTest {
         // Actual Mocking: if and when you encounter a call ticketRepository.findById(12L)
         // must return this above ticket object instead of going to DB
         // this is virtual record used only for testing purpose
-        Mockito.when(ticketRepository.findById(12L)).thenReturn(Optional.of(ticket));
+        when(ticketRepository.findById(12L)).thenReturn(Optional.of(ticket));
 
         //preparing dto to check whether dto is also working properly in service class
         TicketResDto dto = new TicketResDto(
@@ -63,5 +66,19 @@ public class TicketServiceTest {
         //checking whether ticketRepository is called only specific no. of times
         //ideal for fast API Processing
         Mockito.verify(ticketRepository,Mockito.times(2)).findById(12L);
+    }
+
+    @Test
+    public void getTicketByIdWhenNotFound(){
+        //must return empty object
+        when(ticketRepository.findById(12L)).thenReturn(Optional.empty());
+
+        //making call and verifying if exception is thrown
+        Exception e = Assertions.assertThrows(ResourceNotFoundException.class,()->{
+            ticketService.getTicketById(12L);
+        });
+
+        //check for exception message
+        Assertions.assertEquals("Invalid id given",e.getMessage());
     }
 }
