a6ce51f0788c7a20f18052b880031e521f6821bf
Talha <pateltalha934@gmail.com>
2026-03-31 11:53:12 +0530

    Testing + mocking - get by id when found

---
+++ Spring_Boot/MyApp/src/test/java/com/springboot/myapp/service/TicketServiceTest.java
@@ -0,0 +1,67 @@
+package com.springboot.myapp.service;
+
+import com.springboot.myapp.dto.TicketResDto;
+import com.springboot.myapp.enums.TicketPriority;
+import com.springboot.myapp.enums.TicketStatus;
+import com.springboot.myapp.model.Ticket;
+import com.springboot.myapp.repository.TicketRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+public class TicketServiceTest {
+    @InjectMocks
+    private TicketService ticketService;
+    @Mock
+    private TicketRepository ticketRepository;
+
+    @Test
+    public void getTicketByIdWhenExists(){
+        //check if ticket service is not null
+        Assertions.assertNotNull(ticketService);
+
+        //preparing obj for testing
+        Ticket ticket = new Ticket();
+        ticket.setId(12L);
+        ticket.setSubject("test");
+        ticket.setTicketPriority(TicketPriority.LOW);
+        ticket.setTicketStatus(TicketStatus.OPEN);
+        ticket.setCreatedAt(Instant.now());
+
+        // Actual Mocking: if and when you encounter a call ticketRepository.findById(12L)
+        // must return this above ticket object instead of going to DB
+        // this is virtual record used only for testing purpose
+        Mockito.when(ticketRepository.findById(12L)).thenReturn(Optional.of(ticket));
+
+        //preparing dto to check whether dto is also working properly in service class
+        TicketResDto dto = new TicketResDto(
+                ticket.getId(),
+                ticket.getSubject(),
+                ticket.getTicketPriority(),
+                ticket.getTicketStatus(),
+                ticket.getCreatedAt()
+        );
+        TicketResDto dto1 = new TicketResDto(
+                ticket.getId(),
+                ticket.getSubject(),
+                TicketPriority.HIGH,
+                ticket.getTicketStatus(),
+                ticket.getCreatedAt()
+        );
+
+        Assertions.assertEquals(dto,ticketService.getTicketById(12L));
+        Assertions.assertNotEquals(dto1,ticketService.getTicketById(12L));
+
+        //checking whether ticketRepository is called only specific no. of times
+        //ideal for fast API Processing
+        Mockito.verify(ticketRepository,Mockito.times(2)).findById(12L);
+    }
+}
