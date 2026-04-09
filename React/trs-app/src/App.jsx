e0b62d73823ca98acbd7c94d5311fed83634316e
Talha <pateltalha934@gmail.com>
2026-04-09 10:33:16 +0530

    Project setup + array render component

---
+++ React/trs-app/src/App.jsx
@@ -0,0 +1,13 @@
+import TicketList from "./components/ticket-list"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Welcome to React!!!</h1>
+      <TicketList/>
+    </div>
+  )
+}
+
+export default App
---
+++ React/trs-app/src/components/ticket-list.jsx
@@ -0,0 +1,40 @@
+function TicketList(){
+
+    const tickets = [
+    {
+        id: 1,
+        subject: 'Internet down',
+        details: 'Some details',
+        priority: 'HIGH',
+        status: 'OPEN'
+    },
+    {
+        id: 2,
+        subject: 'Internet slow',
+        details: 'Some details',
+        priority: 'MEDIUM',
+        status: 'CLOSED'
+    },
+    {
+        id: 3,
+        subject: 'Internet dead',
+        details: 'Some details',
+        priority: 'HIGH',
+        status: 'OPEN'
+    }]
+
+    return(
+        <div>
+            <h2>Ticket List</h2>
+            {
+                tickets.map((ticket,index)=>(    //Each child in a list should have a unique "key" prop.
+                    <li>
+                        {index+1}. {ticket.subject} -- {ticket.priority} -- {ticket.status}
+                    </li>
+                ))
+            }
+        </div>
+    )
+}
+
+export default TicketList
