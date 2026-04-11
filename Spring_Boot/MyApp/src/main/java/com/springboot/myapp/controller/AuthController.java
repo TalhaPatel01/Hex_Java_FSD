f0df8d82d5a7d2de16bb59e038da6c3ad9e88a4f
Talha <pateltalha934@gmail.com>
2026-04-11 11:49:46 +0530

    Added cross origin

--- Spring_Boot/MyApp/src/main/java/com/springboot/myapp/controller/AuthController.java
+++ Spring_Boot/MyApp/src/main/java/com/springboot/myapp/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.springboot.myapp.utility.JwtUtility;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173/")
 public class AuthController {
 
     private final JwtUtility jwtUtility;
