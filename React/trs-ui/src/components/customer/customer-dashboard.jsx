fde0349d0792d4f8b86b6f9b228fd2167e82f32d
Talha <pateltalha934@gmail.com>
2026-04-13 11:25:25 +0530

    UI loginout and navbar

--- React/trs-ui/src/components/customer/customer-dashboard.jsx
+++ React/trs-ui/src/components/customer/customer-dashboard.jsx
@@ -1,7 +1,40 @@
-function CustomerDashboard(){
-    return(
-        <div>
-            <p>Customer</p>
+import { useNavigate } from "react-router-dom"
+import NavBar from "./navbar"
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+function CustomerDashboard() {
+    const [customer, setCustomer] = useState(undefined)
+    const navigate = useNavigate()
+
+    const getApi = "http://localhost:8080/api/customer/get-one"
+
+    useEffect(() => {
+        const fetchCustomer = async () => {
+            const config = {
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem("token")
+                }
+            }
+            try {
+                const response = await axios.get(getApi,config)
+                setCustomer(response.data)
+            }
+            catch (err) {
+                navigate("/log-in")
+            }
+        }
+
+        fetchCustomer()
+    }, [])
+
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-lg-12">
+                    <NavBar />
+                </div>
+            </div>
         </div>
     )
 }
---
+++ React/trs-ui/src/components/customer/navbar.jsx
@@ -0,0 +1,38 @@
+import { Link, useNavigate } from "react-router-dom"
+
+function NavBar() {
+    const navigate = useNavigate()
+
+    const logout = ()=>{
+        localStorage.clear()
+        navigate("/log-in")
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to='/customer-dashboard'>TRS</Link>
+
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" to="/customer-dashboard">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/tickets">Tickets</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/plans">Plans</Link>
+                        </li>
+
+                    </ul>
+                    <form className="d-flex">
+                        <button className="btn btn-outline-success" onClick={logout}>Logout</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
